4cbdccbbf20a3ea254c823cb7004276ae372cc97
Kartik.Sagar <Kartik.Sagar@IN-LAP-KARTIK>
2026-07-22 19:34:25 +0530

    Spread sample fixes

--- Samples/Samples/Styling.xaml.cs
+++ Samples/Samples/Styling.xaml.cs
@@ -40,21 +40,29 @@ namespace AlphaXSpreadSamplesExplorer.Samples
 
             // Headers Styling
             string headerStyleName = "HeaderStyle";
-            var headerStyle = new Style();
-            worksheet.WorkBook.AddNamedStyle(headerStyleName, headerStyle);
-            headerStyle.BackColor = Color.White;
-            headerStyle.ForeColor = Color.Black;
-            headerStyle.HorizontalAlignment = AlphaXHorizontalAlignment.Center;
+
+            if (worksheet.WorkBook.GetNamedStyle(headerStyleName) == null)
+            {
+                var headerStyle = new Style();
+                worksheet.WorkBook.AddNamedStyle(headerStyleName, headerStyle);
+                headerStyle.BackColor = Color.White;
+                headerStyle.ForeColor = Color.Black;
+                headerStyle.HorizontalAlignment = AlphaXHorizontalAlignment.Center;      
+            }
+
             worksheet.ColumnHeaders.Rows[0].StyleName = headerStyleName;
             worksheet.RowHeaders.Columns[0].StyleName = headerStyleName;
             worksheet.TopLeft.StyleName = headerStyleName;
 
-            // Alternating Row Styling
             string altRowStyle = "AlternatingStyle";
-            var rowStyle = new Style();
-            worksheet.WorkBook.AddNamedStyle(altRowStyle, rowStyle);
-            rowStyle.BackColor = Color.FromArgb(255, 240, 240, 240);
-            rowStyle.ForeColor = Color.Black;
+
+            if (worksheet.WorkBook.GetNamedStyle(altRowStyle) == null)
+            {
+                var rowStyle = new Style();
+                worksheet.WorkBook.AddNamedStyle(altRowStyle, rowStyle);
+                rowStyle.BackColor = Color.FromArgb(255, 240, 240, 240);
+                rowStyle.ForeColor = Color.Black;
+            }
 
             for (int row = 0; row < worksheet.RowCount; row++)
             {
@@ -71,21 +79,31 @@ namespace AlphaXSpreadSamplesExplorer.Samples
 
             // Headers Styling
             string headerStyleName = "HeaderStyle";
-            var headerStyle = new Style();
-            worksheet.WorkBook.AddNamedStyle(headerStyleName, headerStyle);
-            headerStyle.BackColor = Color.FromArgb(255, 131, 84, 139);
-            headerStyle.ForeColor = Color.White;
-            headerStyle.HorizontalAlignment = AlphaXHorizontalAlignment.Center;
+
+            if (worksheet.WorkBook.GetNamedStyle(headerStyleName) == null)
+            {
+                var headerStyle = new Style();
+                worksheet.WorkBook.AddNamedStyle(headerStyleName, headerStyle);
+                headerStyle.BackColor = Color.FromArgb(255, 131, 84, 139);
+                headerStyle.ForeColor = Color.White;
+                headerStyle.HorizontalAlignment = AlphaXHorizontalAlignment.Center;
+                
+            }
+
             worksheet.ColumnHeaders.Rows[0].StyleName = headerStyleName;
             worksheet.RowHeaders.Columns[0].StyleName = headerStyleName;
             worksheet.TopLeft.StyleName = headerStyleName;
 
             // Alternating Row Styling
             string altRowStyle = "AlternatingStyle";
-            var rowStyle = new Style();
-            worksheet.WorkBook.AddNamedStyle(altRowStyle, rowStyle);
-            rowStyle.BackColor = Color.FromArgb(255, 238, 232, 246);
-            rowStyle.ForeColor = Color.Black;
+
+            if(worksheet.WorkBook.GetNamedStyle(altRowStyle) == null)
+            {
+                var rowStyle = new Style();
+                worksheet.WorkBook.AddNamedStyle(altRowStyle, rowStyle);
+                rowStyle.BackColor = Color.FromArgb(255, 238, 232, 246);
+                rowStyle.ForeColor = Color.Black;           
+            }
 
             for (int row = 0; row < worksheet.RowCount; row++)
             {
@@ -103,37 +121,50 @@ namespace AlphaXSpreadSamplesExplorer.Samples
 
             // Headers Styling
             string headerStyleName = "HeaderStyle";
-            var headerStyle = new Style();
-            worksheet.WorkBook.AddNamedStyle(headerStyleName, headerStyle);
-            headerStyle.BackColor = Color.FromArgb(255, 44, 62, 80);
-            headerStyle.ForeColor = Color.White;
-            headerStyle.HorizontalAlignment = AlphaXHorizontalAlignment.Center;
+
+            if (worksheet.WorkBook.GetNamedStyle(headerStyleName) == null)
+            {
+                var headerStyle = new Style();
+                worksheet.WorkBook.AddNamedStyle(headerStyleName, headerStyle);
+                headerStyle.BackColor = Color.FromArgb(255, 44, 62, 80);
+                headerStyle.ForeColor = Color.White;
+                headerStyle.HorizontalAlignment = AlphaXHorizontalAlignment.Center;            
+            }
+
             worksheet.ColumnHeaders.Rows[0].StyleName = headerStyleName;
             worksheet.RowHeaders.Columns[0].StyleName = headerStyleName;
             worksheet.TopLeft.StyleName = headerStyleName;
 
             // Alternating Row Styling
             string altRowStyle = "AlternatingStyle";
-            var rowStyle = new Style();
-            worksheet.WorkBook.AddNamedStyle(altRowStyle, rowStyle);
-            rowStyle.BackColor = Color.FromArgb(255, 22, 160, 133);
-            rowStyle.ForeColor = Color.White;
+
+            if (worksheet.WorkBook.GetNamedStyle(altRowStyle) == null)
+            {
+                var rowStyle = new Style();
+                worksheet.WorkBook.AddNamedStyle(altRowStyle, rowStyle);
+                rowStyle.BackColor = Color.FromArgb(255, 44, 62, 80);
+                rowStyle.ForeColor = Color.White;
+            }
 
             string altRow2Style = "Alternating2Style";
-            var row2Style = new Style();
-            worksheet.WorkBook.AddNamedStyle(altRow2Style, row2Style);
-            row2Style.BackColor = Color.FromArgb(255, 44, 62, 80);
-            row2Style.ForeColor = Color.White;
+
+            if (worksheet.WorkBook.GetNamedStyle(altRow2Style) == null)
+            {
+                var row2Style = new Style();
+                worksheet.WorkBook.AddNamedStyle(altRow2Style, row2Style);
+                row2Style.BackColor = Color.FromArgb(255, 44, 62, 80);
+                row2Style.ForeColor = Color.White;
+            }
 
             for (int row = 0; row < worksheet.RowCount; row++)
             {
                 if (row % 2 != 0)
                 {
-                    worksheet.Rows[row].StyleName = altRow2Style;
+                    worksheet.Rows[row].StyleName = altRowStyle;
                 }
                 else
                 {
-                    worksheet.Rows[row].StyleName = altRowStyle;
+                    worksheet.Rows[row].StyleName = altRow2Style;
                 }
             }
         }
@@ -144,21 +175,30 @@ namespace AlphaXSpreadSamplesExplorer.Samples
 
             // Headers Styling
             string headerStyleName = "HeaderStyle";
-            var headerStyle = new Style();
-            worksheet.WorkBook.AddNamedStyle(headerStyleName, headerStyle);
-            headerStyle.BackColor = Color.FromArgb(255, 188, 221, 255);
-            headerStyle.ForeColor = Color.Black;
-            headerStyle.HorizontalAlignment = AlphaXHorizontalAlignment.Center;
+
+            if (worksheet.WorkBook.GetNamedStyle(headerStyleName) == null)
+            {
+                var headerStyle = new Style();
+                worksheet.WorkBook.AddNamedStyle(headerStyleName, headerStyle);
+                headerStyle.BackColor = Color.FromArgb(255, 188, 221, 255);
+                headerStyle.ForeColor = Color.Black;
+                headerStyle.HorizontalAlignment = AlphaXHorizontalAlignment.Center;             
+            }
+
             worksheet.ColumnHeaders.Rows[0].StyleName = headerStyleName;
             worksheet.RowHeaders.Columns[0].StyleName = headerStyleName;
             worksheet.TopLeft.StyleName = headerStyleName;
 
-            // Alternating Row Styling
             string altRowStyle = "AlternatingStyle";
-            var rowStyle = new Style();
-            worksheet.WorkBook.AddNamedStyle(altRowStyle, rowStyle);
-            rowStyle.BackColor = Color.FromArgb(255, 216, 225, 240);
-            rowStyle.ForeColor = Color.Black;
+
+            if (worksheet.WorkBook.GetNamedStyle(altRowStyle) == null)
+            {
+                var rowStyle = new Style();
+                worksheet.WorkBook.AddNamedStyle(altRowStyle, rowStyle);
+                rowStyle.BackColor = Color.FromArgb(255, 216, 225, 240);
+                rowStyle.ForeColor = Color.Black;
+            }
+
 
             for (int row = 0; row < worksheet.RowCount; row++)
             {
